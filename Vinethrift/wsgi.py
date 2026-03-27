5434a78f6ff48ec0e0625d47396660e557365d6c
Samuelcheche <Samuelonyango932@gmail.com>
2026-03-27 19:46:32 +0300

    new commit

--- Vinethrift/wsgi.py
+++ Vinethrift/wsgi.py
@@ -9,8 +9,28 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vinethrift.settings')
+
+
+def _env_true(name, default=False):
+	value = os.getenv(name)
+	if value is None:
+		return default
+	return value.strip().lower() in {"1", "true", "yes", "on"}
+
+
+def _run_startup_migrations():
+	# Render sets the RENDER environment variable for deployed services.
+	auto_migrate = _env_true("AUTO_MIGRATE", bool(os.getenv("RENDER")))
+	if not auto_migrate:
+		return
+
+	call_command("migrate", interactive=False, run_syncdb=True, verbosity=0)
+
+
+_run_startup_migrations()
 
 application = get_wsgi_application()
